affb09fffe2b7811f3bde2b068953a7e6b1ecfe6
Kwakye <isaaclove520@gmail.com>
2024-01-06 18:35:03 -0800

    prints all functions in a file defined

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden _4 import *
-    allf = dir()
-    for i in range(0), len(allf)):
-        if allf[i][:2] != "__":
-            print("{:s}".format(allf[i]))
+    """Prints all names defined by hidden_4 module."""
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
